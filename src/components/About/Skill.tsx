3abf7e59166c3d57228f18bf858a792a35bcb0ea
Spade, Sean <sspade@axcess-financial.com>
2020-06-07 17:23:56 -0400

    chore: Formatting changes

--- src/components/About/Skill.tsx
+++ src/components/About/Skill.tsx
@@ -4,9 +4,9 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core"
 
 const Skill = ({ text, icon }: { text: string; icon: IconProp }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-1/3 p-8">
       <FontAwesomeIcon fixedWidth icon={icon} className="mx-auto" size={"2x"} />
-      <div className="ml-1 mt-2">{text}</div>
+      <div className="text-center">{text}</div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { Menu } from "../components/Header/Menu"
 import { faReact } from "@fortawesome/free-brands-svg-icons/faReact"
 import { faAws } from "@fortawesome/free-brands-svg-icons/faAws"
 import { faTailwind } from "../images/tailwind"
-import ExercismIcon from "../images/exercism"
 import Skill from "../components/About/Skill"
 import { faGithubAlt } from "@fortawesome/free-brands-svg-icons"
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn"
@@ -14,20 +13,25 @@ function Index(): ReactElement {
       <Menu />
       <div className="h-screen">
         <div className="container mx-auto flex flex-col mt-40 mb-20">
-          <h1 className="font-semibold text-center">Hey, I'm Sean Spade</h1>
-          <p className="text-center">
-            I'm a full-stack developer, who likes stuff 👍
+          <h1 className="font-semibold text-center text-4xl">
+            Hey, I'm Sean Spade
+          </h1>
+          <p className="text-center text-lg mt-8">
+            I believe technology can empower people, by providing and unlocking
+            abilities that were not possible before on scales only limited by
+            imagination.
           </p>
         </div>
         <div className="flex flex-col bg-green-200 py-10">
           <div className="flex justify-center pt-5">
-            <h1 className="font-semibold">Here is some stuff I do</h1>
+            <h1 className="font-semibold">Here is some stuff I like</h1>
           </div>
-          <div className="container flex flex-col mx-auto mt-24">
-            <div className="flex justify-around">
-              <Skill text={"I like react"} icon={faReact} />
-              <Skill text={"I like AWS"} icon={faAws} />
-              <Skill text={"I like Tailwind"} icon={faTailwind} />
+          <div className="container flex flex-col mx-auto mt-16">
+            <div className="flex flex-wrap justify-around ">
+              <Skill text={"React"} icon={faReact} />
+              <Skill text={"AWS"} icon={faAws} />
+              <Skill text={"Tailwind"} icon={faTailwind} />
+              {/*<Skill text={"NodeJS"} icon={faNodeJs} />*/}
             </div>
           </div>
         </div>
@@ -39,7 +43,6 @@ function Index(): ReactElement {
             <div className="flex justify-around">
               <Skill text={"Github"} icon={faGithubAlt} />
               <Skill text={"Linkedin"} icon={faLinkedinIn} />
-              <ExercismIcon height="54" width="54" />
             </div>
           </div>
         </div>
