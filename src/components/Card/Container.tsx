faac0663acc189b8092e6d489da1f7a05b6a24d3
seanspade <skspade@pm.me>
2020-09-29 14:11:29 -0400

    fix: Fixes header link states for currently selected

--- src/components/Card/Container.tsx
+++ src/components/Card/Container.tsx
@@ -4,7 +4,7 @@ interface Props {
   children: ReactNode
 }
 const Container = ({ children }: Props) => {
-  return <div className="flex flex-wrap justify-center">{children}</div>
+  return <div className="flex flex-wrap justify-center pt-20">{children}</div>
 }
 
 export default Container
--- src/components/Header/Link.tsx
+++ src/components/Header/Link.tsx
@@ -1,18 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
+import { LinkGetProps } from "reach__router"
 
 interface Props {
   to: string
   children: string
 }
+
 const CustomLink = ({ to, children }: Props) => {
+  const isActive = ({ isCurrent }: LinkGetProps) => {
+    return isCurrent
+      ? {
+          className:
+            "block mt-4 inline-block mt-0 text-black font-mono mr-4 no-underline",
+        }
+      : {
+          className:
+            "block mt-4 inline-block mt-0 text-green-300 hover:text-black font-mono mr-4 no-underline",
+        }
+  }
   return (
-    <Link
-      to={to}
-      className="block mt-4 inline-block mt-0 text-green-300 hover:text-black font-mono mr-4 no-underline"
-      activeClassName={"text-black"}
-      partiallyActive={true}
-    >
+    <Link to={to} getProps={isActive}>
       {children}
     </Link>
   )
